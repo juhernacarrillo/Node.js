e751143cee1bae8799a863a7c6d1854975a15fb7
juhernacarrillo <mailboxmx@yahoo.com.mx>
2020-10-01 21:36:51 -0500

    Added message

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const nemo = ['nemo'];
 
 function findNemo(array){
     for(let i=0; i<array.length; i++){
+        console.log(array[i]);
         if(array[i]=='nemo'){
             console.log('Found NEMO!');
         }
     }
 
 }
+
+findNemo(nemo);
